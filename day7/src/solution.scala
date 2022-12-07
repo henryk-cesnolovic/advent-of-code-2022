3fd9e5f212b0610a68d52ce1caa9f71f8508732d
Henryk Cesnolovic <unxe@Henryks-MacBook-Air.local>
2022-12-07 12:56:15 +0200

    Advent of Code - day7 // data calculation moved to trait

--- day7/src/solution.scala
+++ day7/src/solution.scala
@@ -3,19 +3,9 @@ import scala.math._
 import scala.annotation.tailrec
 import scala.collection.mutable.Stack
 
-object Solution1 {
-  def main(args: Array[String]): Unit = println(s"Day 1 solution 1: ${solution}.")
-
-  def solution: Long = {
-    val collectedSums = collectSums()
-    collectedSums
-      .map{ case(k, v) => v}
-      .filter(_ <= 100000L)
-      .reduce(_ + _)
-  }
-
+sealed trait Solution {
   @tailrec
-  def collectSums(sums: Map[String, Long] = Map(), nestedFolders: List[String]= List()): Map[String, Long] = {
+  final def collectSums(sums: Map[String, Long] = Map(), nestedFolders: List[String]= List()): Map[String, Long] = {
     val line = StdIn.readLine
     line match {
       case null => return sums
@@ -41,7 +31,19 @@ object Solution1 {
   }
 }
 
-object Solution2 {
+object Solution1 extends Solution{
+  def main(args: Array[String]): Unit = println(s"Day 1 solution 1: ${solution}.")
+
+  def solution: Long = {
+    val collectedSums = collectSums()
+    collectedSums
+      .map{ case(k, v) => v}
+      .filter(_ <= 100000L)
+      .reduce(_ + _)
+  }
+}
+
+object Solution2 extends Solution{
   def main(args: Array[String]): Unit = println(s"Day 1 solution 2: ${solution}.")
 
   def solution: Long = {
@@ -50,31 +52,5 @@ object Solution2 {
       .map{ case(k, v) => v}
       .filter(v => 70000000L - collectedSums("/") + v >= 30000000L)
       .reduce(_ min _)
-  }
-
-  @tailrec
-  def collectSums(sums: Map[String, Long] = Map(), nestedFolders: List[String]= List()): Map[String, Long] = {
-    val line = StdIn.readLine
-    line match {
-      case null => return sums
-      case line => {
-        val splitted = line.split(" ")
-        if(splitted(0) == "$" && splitted(1) == "cd")
-          if(splitted(2) == "..")
-            collectSums(sums, nestedFolders.tail)
-          else {
-            val path = if(nestedFolders.isEmpty) "/" else nestedFolders.head + splitted(2) + "/"
-            collectSums(sums, path +: nestedFolders)
-          }
-        else if((splitted(0) == "$" && splitted(1) == "ls") || splitted(0) == "dir")
-          collectSums(sums, nestedFolders)
-        else if(splitted(0).forall(Character.isDigit)){
-          val newSums = nestedFolders.map(folder => folder -> (splitted(0).toLong + sums.getOrElse(folder, 0L)))
-          collectSums(sums ++ newSums, nestedFolders)
-        }
-        else
-          collectSums(sums, nestedFolders)
-      }
-    }
   }
 }
